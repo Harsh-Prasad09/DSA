1e2479d16df851b7b7f9c47455ae0240784a6dd7
Harsh Prasad <147262677+Harsh-Prasad09@users.noreply.github.com>
2025-04-02 19:22:58 +0530

    Create 2873. Maximum Value of an Ordered Triplet I.cpp

---
+++ 2873. Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long maxVal = 0ll, maxNum = 0ll, maxDiff = 0ll;
+
+        for (int i = 0; i<nums.size(); i++) {
+            long long num = 1ll * nums[i];
+            maxVal = max(maxVal, maxDiff * num);
+            maxDiff = max(maxDiff, maxNum - num);
+            maxNum = max(maxNum, num);
+        }
+
+        return maxVal;
+    }
+};
